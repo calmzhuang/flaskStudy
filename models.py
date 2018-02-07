ddfa17c6662d65a1d1387df380f2355e385b1bec
zhhan <zhhan@niub.la>
2018-02-07 14:59:16 +0800

    create dba

--- models.py
+++ models.py
@@ -9,6 +9,16 @@ class User(dba.Model):
     id = dba.Column(dba.Integer, primary_key = True)
     nickname = dba.Column(dba.String(64), index = True, unique = True)
     email = dba.Column(dba.String(120), index = True, unique = True)
+    posts = dba.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
+
+class Post(dba.Model):
+    id = dba.Column(dba.Integer, primary_key = True)
+    body = dba.Column(dba.String(140))
+    timestamp = dba.Column(dba.DateTime)
+    user_id = dba.Column(dba.Integer, dba.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post %r>' % (self.body)
