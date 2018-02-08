18ae25b703ed924c4104bc662c69d9a7a017bdc9
zhhan <zhhan@niub.la>
2018-02-08 18:47:42 +0800

    version 20180208

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from flask import Flask
 # from flask.ext.sqlalchemy import SQLAlchemy
 from flask_sqlalchemy import SQLAlchemy
+from hashlib import md5
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -11,6 +12,8 @@ class User(dba.Model):
     nickname = dba.Column(dba.String(64), index = True, unique = True)
     email = dba.Column(dba.String(120), index = True, unique = True)
     posts = dba.relationship('Post', backref='author', lazy='dynamic')
+    about_me = dba.Column(dba.String(140))
+    last_seen = dba.Column(dba.DateTime)
 
     dis_authenticated = True
 
@@ -23,6 +26,9 @@ class User(dba.Model):
             return str(self.id)
         except NameError:
             return str(self.id)  # python 3
+
+    def avatar(self, size):
+        return 'http://www.gravatar.com/avatar/' + md5(self.email).hexdigest() + '?d=mm&s=' + str(size)
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
